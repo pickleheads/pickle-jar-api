24350e381a270ccece215ff12bb7893c562926b2
Derek Grossman <derekgrossman@live.com>
2019-03-24 16:59:48 -0400

    added category field to db schema

--- functions/src/shared/db/schemas/idea.ts
+++ functions/src/shared/db/schemas/idea.ts
@@ -5,8 +5,9 @@ export const ARCHIVED = 'archived';
 
 export const ideaSchema = new mongoose.Schema({
   author: { type: String, default: 'picklehead Jackson' },
+  categories: [{type: String}],
   date: { type: Date, default: Date.now },
-  idea: String,
+  idea: {type: String, required: true},
   status: {
     type: String,
     default: ACTIVE,
